3be6423b32a58c77c8e1bd82fd0c5380e5da6210
OM PRAKASH <og04914@gmail.com>
2026-05-31 13:45:31 +0530

    updated

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import TodoDispatchContext from "../../context/TodoDispatchContext";
 
-function AddTodo({updateList}){
+function AddTodo(){
 
     const [inputText, setInputText] = useState('');
+    const {dispatch} = useContext(TodoDispatchContext);
     return (
         <div>
             <input 
@@ -11,8 +13,8 @@ function AddTodo({updateList}){
                 placeholder="add your next todo..."
                 onChange={e => setInputText(e.target.value)}
             />
-            <button onClick={() => 
-                {updateList(inputText);
+            <button onClick={() => {
+                dispatch({type: 'add_todo', payload: {todoText: inputText}});
                 setInputText('');
             }}>Add</button>
         </div>
